e15d46f0f365d41996a9f56d43253f3310adb68d
Suresh Bandaru <bandarucloudmasters@gmail.com>
2025-12-16 14:05:13 +0530

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -3,8 +3,40 @@ resource "aws_instance" "terraform" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
-    Name      = "terraform"
+    Name      = "terraform-1"
     Terraform = "true"
+  }
+
+  provisioner "local-exec" {
+    command    = "echo ${self.private_ip} > inventory"
+    on_failure = continue
+  }
+
+  provisioner "local-exec" {
+    command = "echo Instance is destroyed"
+    when    = destroy
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo systemctl stop nginx",
+      "echo 'successfully stopped nginx server' "
+    ]
+    when = destroy
   }
 }
 
